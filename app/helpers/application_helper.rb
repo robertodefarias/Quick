122bddc4d3d18e9c007edde59e99ec6cf45027d4
Pedro Abreu <peedroabreu@gmail.com>
2026-03-16 16:42:05 -0300

    Adicionado botao copiar em cada mensagem IA, formulario bottom fixo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,17 @@
 module ApplicationHelper
   def render_markdown(text)
-  renderer = Redcarpet::Render::HTML.new
-  markdown = Redcarpet::Markdown.new(renderer, extensions = {})
-  markdown.render(text).html_safe
+    renderer = Redcarpet::Render::HTML.new(
+      filter_html: true,
+      hard_wrap: true
+    )
+
+    markdown = Redcarpet::Markdown.new(renderer,
+      autolink: true,
+      tables: true,
+      fenced_code_blocks: true,
+      lax_spacing: true
+    )
+
+    markdown.render(text.to_s).html_safe
   end
 end
